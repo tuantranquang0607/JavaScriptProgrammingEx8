326b43e678716cef539f871aaf68a2260eed6bcc
Tuan"Thomas" Tran Quang <tranquangtuan060703@gmail.com>
2023-03-02 14:19:40 -0400

    change03/02/2023

--- app.js
+++ app.js
@@ -18,3 +18,40 @@ function showCheckboxState(evt) {
     const span = document.getElementById('checkboxState');
     span.innerText = evt.target.checked
 }
+
+const rdoTechs=document.getElementsByName('tech');
+const prdo = document.getElementById('radio-target');
+
+for(const rdo of rdoTechs) {
+    rdo.onchange = evt => {
+        // console.log(evt);
+        // console.log(evt.target);
+        // console.log(evt.target.value);
+        prdo.innerText=`Your favourite tech is ${evt.target.value}`
+    }
+}
+
+const vehicleMakesSelect = document.getElementById('vehicleMakes');
+const selectedMake = document.getElementById('selection');
+
+vehicleMakesSelect.addEventListener('change', vehicleMakeChange);
+
+const vehicleMakeChange = (evt) => {
+    const index = ect.target.selectedIndex;
+    console.log(index);
+    const opttionSelecct = evt.target.selectedOptions[0];
+    console.log(opttionSelecct);
+
+    if (index > 0) {
+        selectedMake.innerText = `You select Make: ${optionSelected.text} Id: ${optionSelected.value}`;
+    } else {
+        selectedMake.innerText = 'Please make a selection';
+    }
+}
+
+//vehicleMakesSelect.addEventListener('change', vehicleMakeChange);
+vehicleMakesSelect.onchange = vehicleMakeChange;
+
+function clickMe() {
+    console.log(new Date());
+}
